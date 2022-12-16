a2cbe08173feb16613ac833298d09a77c5a5ed66
sam2813 <shalbinbenny@gmail.com>
2022-12-16 18:26:30 -0500

    Student Class

---
+++ main.py
@@ -0,0 +1,17 @@
+class Student:
+    name = 'John'
+    email = 'john@mail'
+    age = 18
+    grades = 4.0
+
+    def fillForm(self):
+        # Fill the required fields in the function
+
+    def payFee(self):
+        #Give Access to payment Gateway and balance
+
+    def enroll(self):
+        #Enroll into a course
+
+    def login(self):
+        #login to a portal
