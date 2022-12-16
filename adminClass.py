18b06648f50aa7affc3a01b73e321e618a3e6bcf
sam2813 <shalbinbenny@gmail.com>
2022-12-16 18:41:11 -0500

    admin class

---
+++ adminClass.py
@@ -0,0 +1,20 @@
+class Admin:
+    available_seats = 0
+
+    def addUser(self):
+        #add a new user to the system
+
+    def ValidateForms(self):
+        #validate the forms submitted by students
+
+    def documentCollection(self):
+        #collect documents
+
+    def documentVerification(self):
+        #verify documents
+
+    def paymentVerification(self):
+        #verify payment info
+
+    def seatsAllocation(self):
+        #allocate seats to students
