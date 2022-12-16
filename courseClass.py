7f48261873a45233a7ace06c6d7a0ee312ee6c03
sam2813 <shalbinbenny@gmail.com>
2022-12-16 18:50:04 -0500

    course class

---
+++ courseClass.py
@@ -0,0 +1,12 @@
+class Course:
+    name = 'eng'
+    code = 101
+    syllabus = []
+    department = 'liter'
+    instructor = 'john'
+
+    def assignWork(self):
+        #set projects and assignemnts
+
+    def prereq(self):
+        #set prerequisites
