d23721ec60492c0e8072547051136ce53ac5bd7b
sam2813 <shalbinbenny@gmail.com>
2022-12-16 18:47:29 -0500

    teacher class

---
+++ teacherClass.py
@@ -0,0 +1,14 @@
+class Teacher:
+    name = 'John'
+    qualifications = '..'
+    email = 'john@mail'
+    assigned_course = [1,2,3]
+
+    def setSyllabus(self):
+        #set syllabus for the course
+
+    def timeTable(self):
+        #set time table
+
+    def login(self):
+        #login to portal
